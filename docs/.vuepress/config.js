3957e139271c11277c2590e4fa337711c1f19d6d
速赔宝 <84664675+lyl-code@users.noreply.github.com>
2022-05-22 13:31:18 +0800

    配置git

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,24 @@ module.exports = {
     description: 'VuePress搭建Kviewui的组件库文档示例代码',
     base: '/',
     port: '8080',
-    themeConfig: {},
+    themeConfig: {
+        // 配置顶部导航栏
+        nav: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '组件',
+                link: '/components/'
+            }
+        ],
+        // 配置侧边栏
+        sidebar: {
+            '/components/': ['/components/', '/components/select.md']
+        }
+    },
     head: [],
-    plugins: [],
+    plugins: ['demo-container'], // 配置插件
     markdown: {}
 }
---
+++ docs/components/README.md
@@ -0,0 +1,14 @@
+# 安装
+## npm安装
+
+推荐使用 npm 的方式安装，它能更好地和 webpack 打包工具配合使用。
+
+npm i kviewui -S
+
+## CDN
+目前可以通过 unpkg.com/kviewui 获取到最新版本的资源，在页面上引入 js 和 css 文件即可开始使用。
+
+<!-- 引入样式 -->
+<link rel="stylesheet" href="https://unpkg.com/kviewui/lib/theme-chalk/index.css">
+<!-- 引入组件库 -->
+<script src="https://unpkg.com/kviewui/lib/index.js"></script>
