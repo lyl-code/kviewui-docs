209d250a48687613f237537a552594d74a755fb4
速赔宝 <84664675+lyl-code@users.noreply.github.com>
2022-05-22 12:34:33 +0800

    首页初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: '',
+    title: 'Vuepress + Kviewui',
+    description: 'VuePress搭建Kviewui的组件库文档示例代码',
+    base: '/',
+    port: '8080',
+    themeConfig: {},
+    head: [],
+    plugins: [],
+    markdown: {}
+}
